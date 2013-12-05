CyanogenMod/android_hardware_qcom_media

vidc: venc: Add support for AVCProfileConstrainedBaseline
This change adds AVCProfileConstrainedBaseline profile
support for H264 encoder. OMX Client can now set this
profile to get required profile settings in SPS NAL unit
from video component.

Change-Id: Id15f620cd3a39b199b2750db92ac1c23bc083c88

mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -3949,12 +3949,14 @@ OMX_ERRORTYPE omx_video::get_supported_profile_level(OMX_VIDEO_PARAM_PROFILELEVE
             if (profileLevelType->nProfileIndex == 0) {
                 profileLevelType->eProfile = OMX_VIDEO_AVCProfileBaseline;
                 profileLevelType->eLevel   = OMX_VIDEO_AVCLevel52;
-
             } else if (profileLevelType->nProfileIndex == 1) {
                 profileLevelType->eProfile = OMX_VIDEO_AVCProfileMain;
                 profileLevelType->eLevel   = OMX_VIDEO_AVCLevel52;
             } else if (profileLevelType->nProfileIndex == 2) {
                 profileLevelType->eProfile = OMX_VIDEO_AVCProfileHigh;
+                profileLevelType->eLevel   = OMX_VIDEO_AVCLevel52;
+            } else if (profileLevelType->nProfileIndex == 3) {
+                profileLevelType->eProfile = QOMX_VIDEO_AVCProfileConstrainedBaseline;
                 profileLevelType->eLevel   = OMX_VIDEO_AVCLevel52;
             } else {
                 DEBUG_PRINT_LOW("get_parameter: OMX_IndexParamVideoProfileLevelQuerySupported nProfileIndex ret NoMore %lu\n",

codeaurora-unoffical/platform-hardware-qcom-media

mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -4573,6 +4573,10 @@ OMX_ERRORTYPE omx_video::get_supported_profile_level(OMX_VIDEO_PARAM_PROFILELEVE
         profileLevelType->eProfile = OMX_VIDEO_AVCProfileHigh;
         profileLevelType->eLevel   = OMX_VIDEO_AVCLevel51;
       }
+      else if (profileLevelType->nProfileIndex == 3) {
+        profileLevelType->eProfile = QOMX_VIDEO_AVCProfileConstrainedBaseline;
+        profileLevelType->eLevel   = OMX_VIDEO_AVCLevel51;
+      }
       else
       {
         DEBUG_PRINT_LOW("get_parameter: OMX_IndexParamVideoProfileLevelQuerySupported nProfileIndex ret NoMore %d\n",