cdb3b2034bf08329511eafdfe22ef74bb5fa765a
Ashray Kulkarni <ashrayk@codeaurora.org>
2014-05-05 18:24:52 -0700

    vidc: venc: add support to configure initialqp to encoder
    Enable setting initialqp and add support to allow setting initial
    qp values to I p & B frames to encoder session.
    
    Change-Id: I9bef96199c24ac64bff1f92dd6be3f256dc73220

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -1304,6 +1304,20 @@ bool venc_dev::venc_set_param(void *paramData,OMX_INDEXTYPE index )
 
                 break;
             }
+        case QOMX_IndexParamVideoInitialQp:
+            {
+                QOMX_EXTNINDEX_VIDEO_INITIALQP * initqp =
+                    (QOMX_EXTNINDEX_VIDEO_INITIALQP *)paramData;
+                 if (initqp->bEnableInitQp) {
+                    DEBUG_PRINT_LOW("Enable initial QP: %d", (int)initqp->bEnableInitQp);
+                    if(venc_enable_initial_qp(initqp) == false) {
+                       DEBUG_PRINT_ERROR("ERROR: Failed to enable initial QP");
+                       return OMX_ErrorUnsupportedSetting;
+                     }
+                 } else
+                    DEBUG_PRINT_ERROR("ERROR: setting QOMX_IndexParamVideoEnableInitialQp");
+                break;
+            }
         case OMX_QcomIndexEnableSliceDeliveryMode:
             {
                 QOMX_EXTNINDEX_PARAMTYPE* pParam =
@@ -1679,6 +1693,9 @@ void venc_dev::venc_config_print()
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: qpI: %ld, qpP: %ld, qpb: %ld",
             session_qp.iframeqp, session_qp.pframqp,session_qp.bframqp);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: Init_qpI: %ld, Init_qpP: %ld, Init_qpb: %ld",
+            init_qp.iframeqp, init_qp.pframeqp, init_qp.bframeqp);
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: VOP_Resolution: %ld, Slice-Mode: %ld, Slize_Size: %ld",
             voptimecfg.voptime_resolution, multislice.mslice_mode,
@@ -2187,6 +2204,51 @@ bool venc_dev::venc_set_slice_delivery_mode(OMX_U32 enable)
         DEBUG_PRINT_ERROR("Slice_DELIVERY_MODE not enabled");
     }
 
+    return true;
+}
+
+bool venc_dev::venc_enable_initial_qp(QOMX_EXTNINDEX_VIDEO_INITIALQP* initqp)
+{
+    int rc;
+    struct v4l2_control control;
+    struct v4l2_ext_control ctrl[4];
+    struct v4l2_ext_controls controls;
+
+    ctrl[0].id = V4L2_CID_MPEG_VIDC_VIDEO_I_FRAME_QP;
+    ctrl[0].value = initqp->nQpI;
+    ctrl[1].id = V4L2_CID_MPEG_VIDC_VIDEO_P_FRAME_QP;
+    ctrl[1].value = initqp->nQpP;
+    ctrl[2].id = V4L2_CID_MPEG_VIDC_VIDEO_B_FRAME_QP;
+    ctrl[2].value = initqp->nQpB;
+    ctrl[3].id = V4L2_CID_MPEG_VIDC_VIDEO_ENABLE_INITIAL_QP;
+    ctrl[3].value = initqp->bEnableInitQp;
+
+    controls.count = 4;
+    controls.ctrl_class = V4L2_CTRL_CLASS_MPEG;
+    controls.controls = ctrl;
+
+    DEBUG_PRINT_LOW("Calling IOCTL set control for id=%x val=%d, id=%x val=%d, id=%x val=%d, id=%x val=%d",
+                    controls.controls[0].id, controls.controls[0].value,
+                    controls.controls[1].id, controls.controls[1].value,
+                    controls.controls[2].id, controls.controls[2].value,
+                    controls.controls[3].id, controls.controls[3].value);
+
+    rc = ioctl(m_nDriver_fd, VIDIOC_S_EXT_CTRLS, &controls);
+    if (rc) {
+        DEBUG_PRINT_ERROR("Failed to set session_qp %d", rc);
+        return false;
+    }
+
+    init_qp.iframeqp = initqp->nQpI;
+    init_qp.pframeqp = initqp->nQpP;
+    init_qp.bframeqp = initqp->nQpB;
+    init_qp.enableinitqp = initqp->bEnableInitQp;
+
+    DEBUG_PRINT_LOW("Success IOCTL set control for id=%x val=%d, id=%x val=%d, id=%x val=%d, id=%x val=%d",
+                    controls.controls[0].id, controls.controls[0].value,
+                    controls.controls[1].id, controls.controls[1].value,
+                    controls.controls[2].id, controls.controls[2].value,
+                    controls.controls[3].id, controls.controls[3].value);
     return true;
 }
 
