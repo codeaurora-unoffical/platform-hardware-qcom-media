CyanogenMod/android_hardware_qcom_media

mm-video: vidc: Expose OMX_QcomIndexConfigPerfLevel
OMX_QcomIndexConfigPerfLevel is just like OMX_QcomIndexParam\
PerfLevel except configurable in execution state.

CRs-Fixed: 695396
Change-Id: I16bdabb9f07c3cf309ed8723f478cf63dcbd3f2b

mm-core/inc/OMX_QCOMExtns.h
@@ -463,6 +463,8 @@ enum OMX_QCOM_EXTN_INDEXTYPE
 
     OMX_QcomIndexParamSetMVSearchrange = 0x7F00003C,
 
+    OMX_QcomIndexConfigPerfLevel = 0x7F00003D,
+
     /*"OMX.QCOM.index.param.video.LTRCount"*/
     OMX_QcomIndexParamVideoLTRCount = QOMX_IndexParamVideoLTRCount,
 
@@ -849,6 +851,17 @@ typedef struct OMX_QCOM_VIDEO_PARAM_PERF_LEVEL {
     OMX_VERSIONTYPE nVersion;           /** OMX specification version information */
     QOMX_VIDEO_PERF_LEVEL ePerfLevel;   /** Performance level */
 } OMX_QCOM_VIDEO_PARAM_PERF_LEVEL;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexConfigPerfLevel extension. It will set
+ * the performance mode specified as QOMX_VIDEO_PERF_LEVEL.
+ */
+typedef struct OMX_QCOM_VIDEO_CONFIG_PERF_LEVEL {
+    OMX_U32 nSize;                      /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;           /** OMX specification version information */
+    QOMX_VIDEO_PERF_LEVEL ePerfLevel;   /** Performance level */
+} OMX_QCOM_VIDEO_CONFIG_PERF_LEVEL;
 
 /**
  * This structure describes the parameters corresponding

codeaurora-unoffical/platform-hardware-qcom-media

mm-core/inc/OMX_QCOMExtns.h
@@ -455,6 +455,8 @@ enum OMX_QCOM_EXTN_INDEXTYPE
     OMX_QcomIndexParamH264VUITimingInfo = 0x7F00003A,
 
     OMX_QcomIndexParamPeakBitrate = 0x7F00003B,
+
+    OMX_QcomIndexConfigPerfLevel = 0x7F00003C,
 };
 
 /**
@@ -782,6 +784,17 @@ typedef struct OMX_QCOM_VIDEO_PARAM_PERF_LEVEL {
     OMX_VERSIONTYPE nVersion;           /** OMX specification version information */
     QOMX_VIDEO_PERF_LEVEL ePerfLevel;   /** Performance level */
 } OMX_QCOM_VIDEO_PARAM_PERF_LEVEL;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexConfigPerfLevel extension. It will set
+ * the performance mode specified as QOMX_VIDEO_PERF_LEVEL.
+ */
+typedef struct OMX_QCOM_VIDEO_CONFIG_PERF_LEVEL {
+    OMX_U32 nSize;                      /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;           /** OMX specification version information */
+    QOMX_VIDEO_PERF_LEVEL ePerfLevel;   /** Performance level */
+} OMX_QCOM_VIDEO_CONFIG_PERF_LEVEL;
 
 /**
  * This structure describes the parameters corresponding