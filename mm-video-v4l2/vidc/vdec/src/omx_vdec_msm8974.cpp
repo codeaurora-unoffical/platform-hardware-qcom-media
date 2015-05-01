7ff91c0900183bab32e7eb781b1b3809b08a3dec
Praneeth Paladugu <ppaladug@codeaurora.org>
2015-05-01 16:46:09 -0700

    mm-video-v4l2: vidc: vdec: Disable S3D for secure playback
    S3D SEI is copied from input to output port in FW. This copying is not
    supported in secure mode. Hence FW is returning SESSION_ERROR. To fix
    this, don't enable S3D extradata for secure playback.
    
    Change-Id: I4dc81e3b1f1d3f90cc248ca95b9adb2c7bceea90

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -9395,16 +9395,23 @@ OMX_ERRORTYPE omx_vdec::enable_extradata(OMX_U32 requested_extradata,
                 DEBUG_PRINT_HIGH("Failed to set timeinfo extradata");
             }
         }
-        if (requested_extradata & OMX_FRAMEPACK_EXTRADATA) {
+
+        /*
+         * S3D extradata for secure playback is not supported by FW. S3D is just
+         * copying the SEI from input to output, which is not allowed in secure
+         * mode.
+         */
+
+        if (!secure_mode && requested_extradata & OMX_FRAMEPACK_EXTRADATA) {
             if (output_capability == V4L2_PIX_FMT_H264) {
-                DEBUG_PRINT_HIGH("enable OMX_FRAMEPACK_EXTRADATA");
+                DEBUG_PRINT_LOW("enable OMX_FRAMEPACK_EXTRADATA");
                 control.id = V4L2_CID_MPEG_VIDC_VIDEO_EXTRADATA;
                 control.value =  V4L2_MPEG_VIDC_EXTRADATA_S3D_FRAME_PACKING;
                 if (ioctl(drv_ctx.video_driver_fd, VIDIOC_S_CTRL, &control)) {
                     DEBUG_PRINT_HIGH("Failed to set S3D_FRAME_PACKING extradata");
                 }
             } else {
-                DEBUG_PRINT_HIGH("OMX_FRAMEPACK_EXTRADATA supported for H264 only");
+                DEBUG_PRINT_LOW("OMX_FRAMEPACK_EXTRADATA supported for H264 only");
             }
         }
         if (requested_extradata & OMX_QP_EXTRADATA) {
