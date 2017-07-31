c8795b93c16f08f9e66ce3c406803a2146c8e2c9
Shiju Mathew <shijum@codeaurora.org>
2017-07-31 19:58:47 -0400

    mm-video-v4l2: Enable output buffer for encoder skipped frames
    Program video driver to send empty output buffer when video
    h/w skips encoding and input buffer due to rate control when
    variable frame rate mode is set. Video h/w sends output buffer
    with zero filled length.
    
    Change-Id: Id2709009f46f94592d0545b18080347a93b556b8

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -6556,6 +6556,20 @@ bool venc_dev::venc_set_ratectrl_cfg(OMX_VIDEO_CONTROLRATETYPE eControlRate)
         rate_ctrl.rcmode = control.value;
     }
 
+    if ((eControlRate == OMX_Video_ControlRateVariableSkipFrames) ||
+        (eControlRate == OMX_Video_ControlRateConstantSkipFrames) ||
+        (eControlRate == QOMX_Video_ControlRateMaxBitrateSkipFrames)) {
+        struct v4l2_control control;
+        int rc = 0;
+        control.id = V4L2_CID_MPEG_VIDC_VIDEO_SEND_SKIPPED_FRAME;
+        control.value = V4L2_MPEG_VIDC_VIDEO_SEND_SKIPPED_FRAME_ENABLE;
+        rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+        if (rc) {
+            DEBUG_PRINT_ERROR("Failed to set control for id=%d, value=%d", control.id, control.value);
+            return false;
+        }
+        DEBUG_PRINT_LOW("Success IOCTL set control for id=%d, value=%d", control.id, control.value);
+    }
 #ifdef _VQZIP_
     if (eControlRate == OMX_Video_ControlRateVariable && (supported_rc_modes & RC_VBR_CFR)
             && m_sVenc_cfg.codectype == V4L2_PIX_FMT_H264) {
