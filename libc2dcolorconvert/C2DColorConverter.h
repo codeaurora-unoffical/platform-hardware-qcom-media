70c2eee43485085eb0836c676de217a92743e5ae
Vikas Reddy Pachika <vpachi@codeaurora.org>
2018-06-04 14:01:10 +0530

    mm-video-v4l2: venc: Add changes for libgbm support
    Add compile time flag for libgbm
    
    Change-Id: I09dbb6b139e8aed64d128b3a7f5944c1e5df5584

--- libc2dcolorconvert/C2DColorConverter.h
+++ libc2dcolorconvert/C2DColorConverter.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012 - 2013, 2015 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012 - 2013, 2018 The Linux Foundation. All rights reserved.
  *
  * redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -39,7 +39,12 @@
 #include <string.h>
 #include <errno.h>
 #include <media/msm_media_info.h>
+#ifdef __LIBGBM__
+#include <gbm.h>
+#include <gbm_priv.h>
+#else
 #include <gralloc_priv.h>
+#endif
 #include <unordered_map>
 
 #include <c2d2.h>
